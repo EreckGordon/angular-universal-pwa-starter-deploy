4e6a7ea74ba57848be687d78edf1007eb83c7baa
EreckGordon <ereckgordon@gmail.com>
2018-02-25 17:48:30 -0700

    auto generated commit. current time: Sun Feb 25 2018 17:48:30 GMT-0700 (US Mountain Standard Time).

--- dist/server/server.js
+++ dist/server/server.js
@@ -41,7 +41,7 @@ const options = {
     allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'x-xsrf-token'],
     credentials: true,
     methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    origin: ['http://localhost:4200', 'http://localhost:8000'],
+    origin: ['http://localhost:4200', 'http://localhost:8000', 'https://universal-demo.ereckgordon.com'],
     preflightContinue: false,
     optionsSuccessStatus: 200,
 };
